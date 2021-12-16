38711b5cd02efb7131bda96cd0694b8a99b6b208
Cassio Neri <cassio.neri@gmail.com>
2021-12-16 18:42:11 +0000

    Changed test.

--- tests/test.cpp
+++ tests/test.cpp
@@ -21,6 +21,7 @@ namespace {
 
 struct ieee32_params_t {
   static auto constexpr exponent_size = uint32_t{8};
+  static auto constexpr exponent_max  = int32_t{255};
   static auto constexpr mantissa_size = uint32_t{23};
   static auto constexpr mantissa_max  = AMARU_POW2(uint32_t, mantissa_size);
 };
@@ -39,40 +40,43 @@ float to_value(ieee32_t const ieee) {
 
 } // namespace <anonymous>
 
-struct float_tests : testing::TestWithParam<int32_t> {
-};
-
-TEST_P(float_tests, test_all_mantissas)
+TEST(amaru_tests, test_all_floats_against_ryu)
 {
-  auto const exponent = GetParam();
-  auto       ieee     = ieee32_t{false, exponent, exponent == 0};
 
-  while (ieee.mantissa != ieee32_params_t::mantissa_max) {
+  auto ieee = ieee32_t{false, 0, 1};
 
-    #if DO_RYU
-      auto ryu = f2d(ieee.mantissa, ieee.exponent);
-      asm("" : "+r"(ieee.mantissa));
-    #endif
-    #if DO_AMARU
-      auto const value = to_value(ieee);
-      auto amaru = amaru_float(value);
-      asm("" : "+r"(amaru.mantissa));
-    #endif
+  for ( ; ieee.exponent < ieee32_params_t::exponent_max; ++ieee.exponent) {
 
     #if DO_RYU && DO_AMARU
-      ASSERT_EQ(ryu.exponent, amaru.exponent) <<
-        "Note: ieee.mantissa = " << ieee.mantissa << ", "
-        "value = " << value;
-
-      ASSERT_EQ(ryu.mantissa, amaru.mantissa) <<
-        "Note: ieee.mantissa = " << ieee.mantissa << ", "
-        "value = " << value;
+      std::cerr << "Exponent: " << ieee.exponent << std::endl;
     #endif
 
-    ++ieee.mantissa;
+    for (ieee.mantissa = ieee.exponent == 0;
+      ieee.mantissa < ieee32_params_t::mantissa_max; ++ieee.mantissa) {
+
+      #if DO_RYU
+        auto ryu = f2d(ieee.mantissa, ieee.exponent);
+        (void) ryu;
+      #endif
+
+      #if DO_AMARU
+        auto const value = to_value(ieee);
+        auto amaru = amaru_float(value);
+        (void) amaru;
+      #else
+        // Silence -Wunused-function
+        (void) to_value;
+      #endif
+
+      #if DO_RYU && DO_AMARU
+        ASSERT_EQ(ryu.exponent, amaru.exponent) <<
+          "Note: ieee.mantissa = " << ieee.mantissa << ", "
+          "value = " << value;
+
+        ASSERT_EQ(ryu.mantissa, amaru.mantissa) <<
+          "Note: ieee.mantissa = " << ieee.mantissa << ", "
+          "value = " << value;
+      #endif
+    }
   }
 }
-
-INSTANTIATE_TEST_SUITE_P(all_exponents, float_tests, \
-    testing::Range(0, 254));
-

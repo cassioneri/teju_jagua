c6adbedb5caf245e2434bca7e45c05688fb55f94
Cassio Neri <cassio.neri@gmail.com>
2021-12-22 01:20:42 +0000

    Cosmetics.

--- src/generator.cpp
+++ src/generator.cpp
@@ -289,7 +289,7 @@ private:
     common_initial(dot_h);
     dot_h <<
       info_.rep() << " to_amaru_dec_" << info_.id() << "(" << info_.rep() <<
-      " const* amaru_bin);\n";
+      " const* binary);\n";
     common_final(dot_h);
   }
 

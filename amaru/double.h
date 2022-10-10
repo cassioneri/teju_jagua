030d25aa107e5b15b689f1a9bf9a22bd0c93b3d4
Cassio Neri <cassio.neri@gmail.com>
2022-10-10 20:24:04 +0100

    Add missing file.

---
+++ amaru/double.h
@@ -0,0 +1,30 @@
+#ifndef AMARU_AMARU_DOUBLE_H_
+#define AMARU_AMARU_DOUBLE_H_
+
+#include "amaru/generated/ieee64_compact.h"
+#include "amaru/generated/ieee64_full.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ieee64_t
+amaru_from_double_to_fields(double value);
+
+ieee64_t
+amaru_from_double_to_decimal_compact(double value);
+
+ieee64_t
+amaru_from_double_to_decimal_full(double value);
+
+uint32_t
+amaru_from_double_to_string_compact(double value, char* str);
+
+uint32_t
+amaru_from_double_to_string_full(double value, char* str);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // AMARU_AMARU_DOUBLE_H_

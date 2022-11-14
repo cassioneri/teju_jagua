f5e518fa31a653668509fbef916ba6bfa830fd48
Cassio Neri <cassio.neri@gmail.com>
2022-11-14 23:15:21 +0000

    Implement log10_075_pow2 and log10_075_pow2_remainder.

--- amaru/common.h
+++ amaru/common.h
@@ -28,7 +28,7 @@ extern "C" {
  */
 static inline
 int32_t log10_pow2(int32_t e) {
-  return (int32_t) ((((uint64_t) 1292913987) * e) >> 32);
+  return (((uint64_t) 1292913987) * e) >> 32;
 }
 
 /**
@@ -40,6 +40,27 @@ int32_t log10_pow2(int32_t e) {
 static inline
 uint32_t log10_pow2_remainder(int32_t e) {
   return ((uint32_t) (((uint64_t) 1292913987) * e)) / 1292913987;
+}
+
+/**
+ * \brief Returns the largest number f such that 10^f <= 0.75 * 2^e.
+ *
+ * \pre -100849 <= e && e < 111481.
+ */
+static inline
+int32_t log10_075_pow2(int32_t e) {
+  return (((uint64_t) 1292913986) * e - 536607788) >> 32;
+}
+
+/**
+ * \brief Returns the remainder r = e - e0, where e0 is the smallest number
+ * such that log10_075_pow2(e0) == log10_pow2(e).
+ *
+ * \pre -112815 <= e && e < 112816.
+ */
+static inline
+uint32_t log10_075_pow2_remainder(int32_t e) {
+  return ((uint32_t) (((uint64_t) 1292913986) * e - 536607788)) / 1292913986;
 }
 
 #ifdef __cplusplus

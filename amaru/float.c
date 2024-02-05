6c3d099030196e152438c0a0eb92e84c765884bb
Cassio Neri <cassio.neri@gmail.com>
2024-02-05 00:31:17 +0000

    Improve float conversion.

--- amaru/float.c
+++ amaru/float.c
@@ -34,14 +34,15 @@ amaru_float_to_ieee32(float const value) {
 amaru32_fields_t
 amaru_ieee32_to_amaru_binary(amaru32_fields_t ieee32) {
 
-  amaru32_fields_t amaru_binary = ieee32;
+  int32_t  e = ieee32.exponent + exponent_min;
+  uint32_t m = ieee32.mantissa;
 
-  amaru_binary.exponent += exponent_min;
   if (ieee32.exponent != 0) {
-    amaru_binary.mantissa += amaru_pow2(uint32_t, mantissa_size);
-    amaru_binary.exponent -= 1;
+    e -= 1;
+    m += amaru_pow2(uint64_t, mantissa_size);
   }
 
+  amaru32_fields_t amaru_binary = { e, m };
   return amaru_binary;
 }
 

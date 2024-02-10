4b3ea7ada0980c1be787b0c4c165270e6aee1b51
Cassio Neri <cassio.neri@gmail.com>
2024-02-10 17:21:41 +0000

    Add mshift ad hoc test.

--- cpp/test/mshift.cpp
+++ cpp/test/mshift.cpp
@@ -6,6 +6,17 @@ namespace {
 
 using namespace amaru::test;
 
+template <typename TImpl>
+void check(amaru_u1_t const m, amaru_u1_t const u, amaru_u1_t const l) {
+
+  amaru_u1_t const actual   = TImpl::mshift(m, u, l);
+  amaru_u1_t const expected = (((amaru_u4_t(u) << amaru_size) + l) * m) >>
+    TImpl::shift;
+
+  EXPECT_EQ(actual, expected) << "Failed for "
+    "m = " << m << ", u = " << u << ", l = " << l << '\n';
+}
+
 using impl_list_t = ::testing::Types<
   built_in_1_t, built_in_1_small_shift_t, 
   synthetic_1_t, synthetic_1_small_shift_t,
@@ -22,21 +33,17 @@ TYPED_TEST_SUITE(mshift, impl_list_t);
 
 TYPED_TEST(mshift, test_one_billion_values) {
 
-  using impl_t = TypeParam;
+  for (amaru_u1_t m = 0; m < 1000; ++m)
+    for (amaru_u1_t u = 0; u < 1000; ++u)
+      for (amaru_u1_t l = 0; l < 1000 && !this->HasFailure(); ++l)
+        check<TypeParam>(m, u, l);
+}
 
-  for (amaru_u1_t m = 0; m < 1000; ++m) {
-    for (amaru_u1_t u = 0; u < 1000; ++u) {
-      for (amaru_u1_t l = 0; l < 1000; ++l) {
-
-        amaru_u1_t const actual   = impl_t::mshift(m, u, l);
-        amaru_u1_t const expected = (((amaru_u4_t(u) << amaru_size) + l) * m) >>
-          impl_t::shift;
-
-        ASSERT_EQ(actual, expected) << "Failed for "
-          "m = " << m << ", u = " << u << ", l = " << l << '\n';
-      }
-    }
-  }
+TYPED_TEST(mshift, ad_hoc) {
+  amaru_u1_t const m = 0;
+  amaru_u1_t const u = 0;
+  amaru_u1_t const l = 0;
+  check<TypeParam>(m, u, l);
 }
 
 } // namespace <anonymous>

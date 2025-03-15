cd12b302d35b27359d019b4d8a1ba265fd6dbb70
Cassio Neri <cassio.neri@gmail.com>
2025-03-15 13:41:12 +0000

    Initial support for x86 extended format.

---
+++ teju/generated/x86_extended.h
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: APACHE-2.0
+// SPDX-FileCopyrightText: 2021-2025 Cassio Neri <cassio.neri@gmail.com>
+
+// This file was generated. DO NOT EDIT IT.
+
+#ifndef TEJU_TEJU_GENERATED_X86_EXTENDED_H_
+#define TEJU_TEJU_GENERATED_X86_EXTENDED_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "teju/config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+teju128_fields_t
+teju_x86_extended(teju128_fields_t binary);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TEJU_TEJU_GENERATED_X86_EXTENDED_H_

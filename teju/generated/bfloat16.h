d3d1d39b6020fae2758c03543ab2e6933858606b
Cassio Neri <cassio.neri@gmail.com>
2025-04-10 17:01:05 +0100

    Add bfloat16.

---
+++ teju/generated/bfloat16.h
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: APACHE-2.0
+// SPDX-FileCopyrightText: 2021-2025 Cassio Neri <cassio.neri@gmail.com>
+
+// This file was generated. DO NOT EDIT IT.
+
+#ifndef TEJU_TEJU_GENERATED_BFLOAT16_H_
+#define TEJU_TEJU_GENERATED_BFLOAT16_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "teju/config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+teju16_fields_t
+teju_bfloat16(teju16_fields_t binary);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TEJU_TEJU_GENERATED_BFLOAT16_H_

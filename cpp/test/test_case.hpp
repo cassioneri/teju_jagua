3c31196b7670e6a8f500f7908d32b5f6ef87b4b0
Cassio Neri <cassio.neri@gmail.com>
2025-08-26 04:35:45 +0100

    Remove unused code.

--- cpp/test/test_case.hpp
+++ cpp/test/test_case.hpp
@@ -12,10 +12,7 @@
 
 #include "common/fields.hpp"
 
-#include <iosfwd>
-
-namespace teju {
-namespace test {
+namespace teju::test {
 
 /**
  * @brief Test case for a given type.
@@ -40,17 +37,6 @@ struct test_case_t {
   explicit test_case_t(T value, decimal_t<T> const& expected);
 
   /**
-   * @brief Constructor from the fields of Tejú Jaguá's binary representation
-   *        and the expected fields of its Tejú Jaguá's decimal representation.
-   *
-   * @param  value          The fields of Tejú Jaguá's binary representation.
-   * @param  expected       The expected fields of Tejú Jaguá's decimal
-   *                        representation.
-   */
-  //explicit test_case_t(decimal_t<T> const& teju_binary,
-  //  decimal_t<T> const& expected);
-
-  /**
    * @brief Gets the value.
    *
    * @returns The value.
@@ -73,38 +59,6 @@ private:
 
 }; // test_case_t
 
-/**
- * @brief Stream operator for test_case_t<T>.
- *
- * @tparam C                The char type.
- * @tparam CT               The char-traits type.
- * @tparam T                The type T.
- *
- * @param os                The object that the test case is streamed to.
- * @param test_case         The test case.
- *
- * @return os.
- */
-template <typename C, typename CT, typename T>
-std::basic_ostream<C, CT>&
-operator <<(std::basic_ostream<C, CT>& os, test_case_t<T> const& test_case) {
-
-//   using traits_t          = teju::traits_t<T>;
-//   using fields_t          = typename test_case_t<T>::fields_t;
-//   using streamable_uint_t = typename fields_t::streamable_uint_t;
-
-//   auto const value        = test_case.value();
-//   auto const ieee         = traits_t::value_to_ieee(value);
-//   auto const teju_binary  = traits_t::ieee_to_teju_binary(ieee);
-
-//   // Specially formatted for looking pretty in gtest messages.
-//   return os << test_case.expected() << "\n"
-//     "    [value = " << streamable_uint_t{teju_binary.c.mantissa} << " * 2^(" <<
-//     teju_binary.c.exponent << ")]";
-  return os;
-}
-
-} // namespace test
-} // namespace teju
+} // namespace teju::test
 
 #endif // TEJU_CPP_TEST_TEST_CASE_HPP_

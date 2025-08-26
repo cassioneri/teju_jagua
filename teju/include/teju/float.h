0d8fad1365e122e8efa4ce6ed65aae78d00e9656
Cassio Neri <cassio.neri@gmail.com>
2025-08-26 10:52:04 +0100

    Increase inlining.

--- teju/include/teju/float.h
+++ teju/include/teju/float.h
@@ -10,7 +10,18 @@
 #ifndef TEJU_TEJU_INCLUDE_TEJU_FLOAT_H_
 #define TEJU_TEJU_INCLUDE_TEJU_FLOAT_H_
 
+#include "teju/src/common.h"
 #include "teju/src/config.h"
+
+#if defined(teju_has_uint128)
+  #include "teju/src/generated/ieee32_with_uint128.h"
+#else
+  #include "teju/src/generated/ieee32_no_uint128.h"
+#endif
+
+#include <assert.h>
+#include <stdint.h>
+#include <string.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -25,8 +36,35 @@ extern "C" {
  *
  * @returns The binary representation of the given value.
  */
+inline
 teju32_fields_t
-teju_float_to_binary(float value);
+teju_float_to_binary(float const value) {
+
+  assert(isfinite(value) && value > 0 && "Invalid float value.");
+
+  typedef teju32_fields_t teju_fields_t;
+  typedef teju32_u1_t     teju_u1_t;
+
+  uint32_t const mantissa_width =  24u;
+  int32_t  const exponent_min   = -149;
+
+  teju_u1_t bits;
+  memcpy(&bits, &value, sizeof(value));
+
+  teju_u1_t mantissa = teju_lsb(teju_u1_t, bits, mantissa_width - 1u);
+  bits >>= (mantissa_width - 1u);
+
+  int32_t exponent = (int32_t) bits;
+
+  if (exponent != 0) {
+    exponent -= 1;
+    mantissa |= teju_pow2(teju_u1_t, mantissa_width - 1u);
+  }
+
+  exponent += exponent_min;
+  teju_fields_t binary = {exponent, mantissa};
+  return binary;
+}
 
 /**
  * @brief Gets the decimal representation of a given value.
@@ -37,8 +75,16 @@ teju_float_to_binary(float value);
  *
  * @returns The decimal representation of the given value.
  */
+inline
 teju32_fields_t
-teju_float_to_decimal(float value);
+teju_float_to_decimal(float const value) {
+  teju32_fields_t binary = teju_float_to_binary(value);
+  #if defined(teju_has_uint128)
+    return teju_ieee32_with_uint128(binary);
+  #else
+    return teju_ieee32_no_uint128(binary);
+  #endif
+}
 
 #ifdef __cplusplus
 }
--- teju/include/teju/float128.h
+++ teju/include/teju/float128.h
@@ -12,7 +12,14 @@
 
 #if defined(teju_has_float128)
 
+#include "teju/src/common.h"
 #include "teju/src/config.h"
+
+#include "teju/src/generated/ieee128.h"
+
+#include <assert.h>
+#include <stdint.h>
+#include <string.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -27,8 +34,35 @@ extern "C" {
  *
  * @returns The binary representation of the given value.
  */
+inline
 teju128_fields_t
-teju_float128_to_binary(float128_t value);
+teju_float128_to_binary(float128_t const value) {
+
+  assert(isfinite(value) && value > 0 && "Invalid float128_t value.");
+
+  typedef teju128_fields_t teju_fields_t;
+  typedef teju128_u1_t     teju_u1_t;
+
+  uint32_t const mantissa_width =    113u;
+  int32_t  const exponent_min   = -16494;
+
+  teju_u1_t bits;
+  memcpy(&bits, &value, sizeof(value));
+
+  teju_u1_t mantissa = teju_lsb(teju_u1_t, bits, mantissa_width - 1u);
+  bits >>= (mantissa_width - 1u);
+
+  int32_t exponent = (int32_t) bits;
+
+  if (exponent != 0) {
+    exponent -= 1;
+    mantissa |= teju_pow2(teju_u1_t, mantissa_width - 1u);
+  }
+
+  exponent += exponent_min;
+  teju128_fields_t binary = {exponent, mantissa};
+  return binary;
+}
 
 /**
  * @brief Gets the decimal representation of a given value.
@@ -39,8 +73,12 @@ teju_float128_to_binary(float128_t value);
  *
  * @returns The decimal representation of the given value.
  */
+inline
 teju128_fields_t
-teju_float128_to_decimal(float128_t value);
+teju_float128_to_decimal(float128_t const value) {
+  teju128_fields_t binary = teju_float128_to_binary(value);
+  return teju_ieee128(binary);
+}
 
 #ifdef __cplusplus
 }

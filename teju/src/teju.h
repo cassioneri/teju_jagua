d98c0c48b31d8649bbb88632b0cdf767f0c7e1e5
Cassio Neri <cassio.neri@gmail.com>
2025-08-27 23:39:41 +0100

    Change definition of a.

--- teju/src/teju.h
+++ teju/src/teju.h
@@ -211,20 +211,21 @@ is_closer_to_left(teju_u1_t const c_2) {
 static inline
 teju_fields_t centred(int32_t const e, teju_u1_t const m) {
 
-  int32_t           const f   = teju_log10_pow2(e);
-  uint32_t          const r   = teju_log10_pow2_residual(e);
-  teju_multiplier_t const M   = multipliers[f - teju_storage_index_offset];
-  teju_u1_t         const m_a = (2u * m - 1u) << r;
-  teju_u1_t         const m_b = (2u * m + 1u) << r;
-  teju_u1_t         const b   = teju_mshift(m_b, M);
-  teju_u1_t         const a   = teju_mshift(m_a, M);
-  teju_u1_t         const q   = teju_div10(b);
-  teju_u1_t         const s   = 10u * q;
+  int32_t           const f        = teju_log10_pow2(e);
+  uint32_t          const r        = teju_log10_pow2_residual(e);
+  teju_multiplier_t const M        = multipliers[f - teju_storage_index_offset];
+  teju_u1_t         const m_b      = (2u * m + 1u) << r;
+  teju_u1_t         const m_a      = (2u * m - 1u) << r;
+  teju_u1_t         const a_is_tie = is_tie(f, m_a);
+  teju_u1_t         const b        = teju_mshift(m_b, M);
+  teju_u1_t         const a        = teju_mshift(m_a, M) + !a_is_tie;
+  teju_u1_t         const q        = teju_div10(b);
+  teju_u1_t         const s        = 10u * q;
 
   bool const shortest =
-    s == a ?  is_tie(f, m_a) && wins_tiebreak(m) :
     s == b ? !is_tie(f, m_b) || wins_tiebreak(m) :
-    a <  s;
+    s == a ? !a_is_tie       || wins_tiebreak(m) :
+    s >  a;
   if (shortest)
     return remove_trailing_zeros(f + 1, q);
 

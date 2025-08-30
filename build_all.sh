cb00a17e3840f5be8feb1eb5cf96db51f1531a9f
Cassio Neri <cassio.neri@gmail.com>
2025-08-30 09:19:06 +0100

    Improve build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,10 +1,14 @@
 set -ex
 
 presets="
+  clang.debug.make
+  clang.debug.ninja
   clang.release.make
   clang.release.ninja
   clang.release-symbols.make
   clang.release-symbols.ninja
+  gcc.debug.make
+  gcc.debug.ninja
   gcc.release.make
   gcc.release.ninja
   gcc.release-symbols.make
@@ -15,5 +19,7 @@ for preset in ${presets}
   do
     cmake --preset ${preset}
     cmake --build build/${preset}
-    ./build/${preset}/bin/test
+    if [[ ${preset} == *".release.ninja" ]]; then
+      ./build/${preset}/bin/test
+    fi
   done

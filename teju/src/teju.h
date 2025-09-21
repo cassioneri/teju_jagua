bbd392044de48f6e76c7ceca0c916ab11dc53d1e
Cassio Neri <cassio.neri@gmail.com>
2025-09-21 20:42:03 +0100

    Use allows_ties for uncentred values.

--- teju/src/teju.h
+++ teju/src/teju.h
@@ -341,11 +341,15 @@ to_decimal_uncentred(int32_t const e) {
 
   if (teju_calculation_sorted || a < b) {
 
-    bool const shortest =
-      s == a ?  is_tie_uncentred(f, m_a) && wins_tiebreak(m) :
-      s == b ? !is_tie_uncentred(f, m_b) || wins_tiebreak(m) :
-      /*else*/ s > a;
-    if (shortest)
+    if (allows_ties(f)) {
+      bool const shortest =
+        s == a ?  is_tie_uncentred(f, m_a) && wins_tiebreak(m) :
+        s == b ? !is_tie_uncentred(f, m_b) || wins_tiebreak(m) :
+        /*else*/ s > a;
+      if (shortest)
+        return remove_trailing_zeros(f + 1, q);
+    }
+    else if (s > a)
       return remove_trailing_zeros(f + 1, q);
 
     // m_c = 4 * m * pow(2, r) = pow(2, teju_mantissa_width + r + 1)
